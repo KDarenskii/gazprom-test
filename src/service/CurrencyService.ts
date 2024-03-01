e966ffd2ca6b356751fc486cbb3227c17f04a4a8
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-01 22:51:44 +0300

    added service for currency request

---
+++ src/service/CurrencyService.ts
@@ -0,0 +1,12 @@
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { api } from '../api';
+
+const ENDPOINT = '/currency';
+
+export class CurrencyService {
+  static getAll = async (
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<CurrencyInfo[]>> => {
+    return api.get(ENDPOINT, config);
+  };
+}
