d76f200c51a785ba2cd13ff67396801dd5a83e55
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-01 22:53:06 +0300

    moved logic to custom hook and made currency request

--- src/components/CurrencyChart/CurrencyChart.tsx
+++ src/components/CurrencyChart/CurrencyChart.tsx
@@ -1,118 +1,44 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { ReactECharts } from '../shared/Echarts/ReactECharts';
+import { Loader } from '@consta/uikit/Loader';
+import { Informer } from '@consta/uikit/Informer';
 
 import './currencyChart.css';
 import { Layout } from '@consta/uikit/Layout';
 import { AverageValueLabel } from './AverageValueLabel';
-import { currencyFormatter } from '../../utils/currencyFormatter';
 import { CurrencyController } from './CurrencyController';
-import { currencySymbols } from '../../constants/currencySymbols';
+import { useCurrencyChart } from './useCurrencyChart';
 
-interface CurrencyChartProps {
-  currencyData: CurrencyInfo[];
-}
+export const CurrencyChart: FC = () => {
+  const {
+    config,
+    averageValue,
+    uniqueIndicators,
+    isLoading,
+    error,
+    currencyIndicator,
+    setCurrencyIndicator,
+  } = useCurrencyChart();
 
-export const CurrencyChart: FC<CurrencyChartProps> = ({ currencyData }) => {
-  const [currencyIndicator, setCurrencyIndicator] = useState<string>(
-    currencyData[0].indicator
-  );
+  if (isLoading) {
+    return <Loader />;
+  }
 
-  const filteredCurrencyByIndicator = currencyData.filter(
-    (currency) => currency.indicator === currencyIndicator
-  );
-
-  const months = Array.from(
-    new Set(filteredCurrencyByIndicator.map(({ month }) => month))
-  );
-
-  const currencies = Array.from(
-    new Set(currencyData.map(({ indicator }) => indicator))
-  );
-
-  const values = filteredCurrencyByIndicator.map((currency) => currency.value);
-
-  const averageValue = (
-    values.reduce((sum, value) => sum + value, 0) / values.length
-  ).toFixed(1);
-
-  const option = {
-    title: {
-      text: `КУРС ДОЛЛАРА, ${currencySymbols[currencyIndicator]}/₽`,
-      textStyle: {
-        fontWeight: 'bold',
-        fontFamily: 'Inter',
-        fontSize: 20,
-        color: '#002033',
-      },
-    },
-    color: '#F38B00',
-    grid: {
-      right: 200,
-    },
-    tooltip: {
-      trigger: 'axis',
-      className: 'currency-tooltip',
-      valueFormatter: (value: string | number): string =>
-        currencyFormatter.format(Number(value)),
-    },
-    xAxis: {
-      type: 'category',
-      axisLabel: {
-        fontFamily: 'Open Sans',
-        fontSize: 10,
-        color: 'rgba(#002033, 0.6)',
-      },
-      data: months,
-      offset: 25,
-      axisTick: {
-        show: false,
-      },
-      axisLine: {
-        show: false,
-      },
-    },
-    yAxis: {
-      type: 'value',
-      scale: true,
-      interval: 3,
-      offset: 17,
-      axisTick: {
-        show: true,
-        inside: false,
-        length: 5,
-        alignWithLabel: true,
-        lineStyle: {
-          color: '#D9D9D9',
-        },
-      },
-      splitLine: {
-        lineStyle: {
-          type: 'dashed',
-        },
-      },
-    },
-    series: [
-      {
-        name: currencyIndicator,
-        type: 'line',
-        data: values,
-        lineStyle: {
-          color: 'orange',
-        },
-        symbol: 'none',
-      },
-    ],
-  };
+  if (error) {
+    return <Informer status="alert" title="Произошла ошибка" label={error} />;
+  }
 
   return (
     <Layout className="chart-container">
-      <ReactECharts option={option} />
+      <ReactECharts option={config} />
       <AverageValueLabel value={averageValue} />
-      <CurrencyController
-        currencies={currencies}
-        value={currencyIndicator}
-        onChange={setCurrencyIndicator}
-      />
+      {currencyIndicator && (
+        <CurrencyController
+          currencies={uniqueIndicators}
+          value={currencyIndicator}
+          onChange={setCurrencyIndicator}
+        />
+      )}
     </Layout>
   );
 };
---
+++ src/components/CurrencyChart/useCurrencyChart.ts
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import { CurrencyService } from '../../service/CurrencyService';
+import { getChartConfig } from './getChartConfig';
+
+export const useCurrencyChart = () => {
+  const [currencyData, setCurrencyData] = useState<CurrencyInfo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const [currencyIndicator, setCurrencyIndicator] = useState<string | null>(
+    null
+  );
+
+  useEffect(() => {
+    const getCurrencyData = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const { data } = await CurrencyService.getAll();
+        setCurrencyData(data);
+        if (data.length > 0) {
+          setCurrencyIndicator(data[0].indicator);
+        }
+      } catch (error) {
+        setError('Не удалось получить данные');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getCurrencyData();
+  }, []);
+
+  const filteredCurrencyByIndicator = currencyData.filter(
+    (currency) => currency.indicator === currencyIndicator
+  );
+
+  const uniqueMonths = Array.from(
+    new Set(filteredCurrencyByIndicator.map(({ month }) => month))
+  );
+
+  const uniqueIndicators = Array.from(
+    new Set(currencyData.map(({ indicator }) => indicator))
+  );
+
+  const currencyValues = filteredCurrencyByIndicator.map(
+    (currency) => currency.value
+  );
+
+  const averageValue = (
+    currencyValues.reduce((sum, value) => sum + value, 0) /
+    currencyValues.length
+  ).toFixed(1);
+
+  const config = getChartConfig({
+    indicator: currencyIndicator,
+    values: currencyValues,
+    months: uniqueMonths,
+  });
+
+  return {
+    isLoading,
+    error,
+    config,
+    averageValue,
+    uniqueIndicators,
+    currencyIndicator,
+    setCurrencyIndicator,
+  };
+};
